1a03b91e7787c7ae5978de4b95163c38c66ee56b
Ashish Ubgade <ubgadeashish@gmail.com>
2024-04-24 12:15:24 -0500

    hehehehe

--- app/src/main/java/edu/utsa/dreamweaver/MainActivity.java
+++ app/src/main/java/edu/utsa/dreamweaver/MainActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.ComponentActivity;
 import androidx.annotation.Nullable;
 
 public class MainActivity extends ComponentActivity {
     Button button;
+    String unameTest = "test";
+    String passTest = "test";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,13 +24,25 @@ public class MainActivity extends ComponentActivity {
 
     //buttons
     private void setupButtons(){
-        //button = (Button) findViewById(R.id.login);
+        button = (Button) findViewById(R.id.login_button);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-                startActivity(intent);
+                EditText uname = (EditText) findViewById(R.id.input_username);
+                EditText upass = (EditText) findViewById(R.id.input_password);
+                if (authenticate(uname.getText().toString(), upass.getText().toString())) {
+                    Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    uname.setError("Username not found");
+                    upass.setError("Incorrect Password");
+                }
             }
         });
+    }
+
+    private boolean authenticate (String username, String password){
+        return username.equalsIgnoreCase(unameTest) && password.equals(passTest);
     }
 }
