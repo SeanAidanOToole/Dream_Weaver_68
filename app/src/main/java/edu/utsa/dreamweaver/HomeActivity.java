b3c8ebc2b726371390f3d651720493684cec97f9
SeanAidanOToole <126743169+SeanAidanOToole@users.noreply.github.com>
2024-05-07 11:16:16 -0500

    Please Work Bruh (#6)
    * All layouts
    
    * Fixed :) (#5)
    
    * Revert "Login and Signup page"
    
    This reverts commit ca259be19e125020784a16bbc8edf3d4c2ee6331.
    
    ---------
    
    Co-authored-by: Arnav Ubgade <117597405+aaubgade@users.noreply.github.com>
    Co-authored-by: SeanJStudent <sean.jauregui@my.utsa.edu>

---
+++ app/src/main/java/edu/utsa/dreamweaver/HomeActivity.java
@@ -0,0 +1,18 @@
+package edu.utsa.dreamweaver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.ComponentActivity;
+import androidx.annotation.Nullable;
+
+public class HomeActivity extends ComponentActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage);
+    }
+}
